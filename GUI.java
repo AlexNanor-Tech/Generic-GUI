24e88650d84bfe95a61f97cd58609c307be97e4b
alexnanor <73364939+AlexNanor-Tech@users.noreply.github.com>
2022-03-26 16:36:05 -0400

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -5,23 +5,35 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.BorderFactory;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 
-public class GUI {
+public class GUI implements ActionListener {
+
+    //State:
+    private int count = 0;
+    private JLabel label;
+    private JFrame frame;
+    private JPanel panel;
+
+
+    //Behaviour:
 
     //Constructor
     public GUI(){
 
         //Step 1: Create resizeable region
-        JFrame frame = new JFrame();
+         frame = new JFrame();
         //Step 2: Create internal portion.
-        JPanel panel = new JPanel();
+         panel = new JPanel();
         //To use an object of the Java swing class. Type the objects name, with a dot, and then the method or variable
         //you'd like to access
         JButton button = new JButton("Push Here"); // If we want our button to count clicks.
+        button.addActionListener(this);
         //One must configure button to count click events.
-        JLabel label = new JLabel("Number of clicks: 0");
+        label = new JLabel("Number of clicks: 0");
         //Use panel to setup border, frame, and layout.
         //border is in units of pixels.
         //Pass a borderfactory to setBorder method of panel object (instance of JPanel Class of JSwing Library)
@@ -35,7 +47,7 @@ public class GUI {
         //Contraints:
         frame.add(panel, BorderLayout.CENTER); // Add Panel to the frame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Set what happens when we close the frame
-        frame.setTitle("My GUI"); //Set title of window
+        frame.setTitle("My GUI"); //Set title of window. Outer window containing titel bar.
         frame.pack(); // Set window to match a certain size
         frame.setVisible(true); // Set the window to be visible and in-focus
 
@@ -53,5 +65,11 @@ public class GUI {
         //Setup:
         new GUI(); // Call our GUI constructor which will set parameters (i.e. do the stuff inside the curly braces)
 
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        count++;
+        label.setText("Number of clicks " + count);
     }
 }
