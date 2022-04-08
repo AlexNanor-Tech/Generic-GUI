a522dc1816e869fea8e9fe73f8d5521e60753f24
alexnanor <73364939+AlexNanor-Tech@users.noreply.github.com>
2022-04-07 23:03:07 -0400

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -24,36 +24,33 @@ public class GUI implements ActionListener {
     //Constructor
     public GUI(){
 
-        //Step 1: Create resizeable region
+        
          frame = new JFrame();
-        //Step 2: Create internal portion.
+       
          panel = new JPanel();
-        //To use an object of the Java swing class. Type the objects name, with a dot, and then the method or variable
-        //you'd like to access
-        JButton button = new JButton("Push Here"); // If we want our button to count clicks.
+     
+        JButton button = new JButton("Push Here"); 
         button.addActionListener(this);
-        //One must configure button to count click events.
+       
         label = new JLabel("Number of clicks: 0");
-        //Use panel to setup border, frame, and layout.
-        //border is in units of pixels.
-        //Pass a borderfactory to setBorder method of panel object (instance of JPanel Class of JSwing Library)
+       
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-        // layout to match what was in the borders
+       
         panel.setLayout(new GridLayout(0,1));
         panel.add(button);
         panel.add(label);
 
 
         //Contraints:
-        frame.add(panel, BorderLayout.CENTER); // Add Panel to the frame
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Set what happens when we close the frame
-        frame.setTitle("My GUI"); //Set title of window. Outer window containing titel bar.
-        frame.pack(); // Set window to match a certain size
-        frame.setVisible(true); // Set the window to be visible and in-focus
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+        frame.setTitle("My GUI");
+        frame.pack(); 
+        frame.setVisible(true); 
 
 
 
-        // Generates window without button/label
+  
 
 
 
@@ -61,9 +58,8 @@ public class GUI implements ActionListener {
 
 
     public static void main(String[] args) {
-        //Swing is how we make GUIs in Java
-        //Setup:
-        new GUI(); // Call our GUI constructor which will set parameters (i.e. do the stuff inside the curly braces)
+       
+        new GUI();
 
     }
 
